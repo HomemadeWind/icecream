5a15d1921cecaf0aa3fc693d086288a40ae623b9
HomemadeWind <125684130+HomemadeWind@users.noreply.github.com>
2023-03-17 23:07:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import pickle
 import sklearn
-import model.pickle
+import streamlit as st
+import numpy as np 
+model=pickle.load(open("model.pickle","rb"))
+st.title("Revenue Prediction")
+t= st.number_input("Input Temperature")
+t=np.array(t)
+t=t.reshape(-1,1)
+if st.button("Predict"):
+  y = model.predict(t)
+  st.success(*y)
